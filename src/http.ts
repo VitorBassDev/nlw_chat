4a0b72b242e6706a03e26aa317281e997db9b155
Vitor Guedes <vittorbassdev@gmail.com>
2021-05-02 18:20:01 -0300

    Refectory do arquivo http

--- src/http.ts
+++ src/http.ts
@@ -1,37 +1,35 @@
-import express from 'express'
-import {createServer} from 'http'
-import {Server, Socket} from 'socket.io'
-import path from 'path'
-import "./database"
-import {routes} from './router'
+import express from "express";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
 
-const app = express()
+import "./database";
+import { routes } from "./router";
 
+const app = express();
 
-// SETAR A CONFIGURAÇÃO DOS ARQUIVOS DO FRONTENT
-app.use(express.static(path.join(__dirname, "..", "public")))
-app.set("views", path.join(__dirname, "..", "public"))
-app.engine("html", require("ejs").renderFile)
-app.set("view engine", "html")
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
-
-app.get("/pages/client", (request, response) =>{
-  return response.render("html/client.html")
-})
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+});
 
 app.get("/pages/admin", (request, response) => {
   return response.render("html/admin.html");
 });
 
-const http = createServer(app) // criar protocolo http
-const io   = new Server(http)  // criar protoloco WS
+const http = createServer(app); // Criando protocolo http
+const io = new Server(http); // Criando protocolo ws
 
 io.on("connection", (socket: Socket) => {
-  console.log('Conexão com o ', socket.id)
-})
+  // console.log("Se conectou", socket.id);
+});
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(routes)
+app.use(routes);
 
-export{http, io}
+export { http, io };
